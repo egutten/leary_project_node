ccb1dbbcb89099b99f8552678162848484ffa3be
egutten <guttenberg.emily@gmail.com>
2019-09-23 08:16:38 -0700

    snippet fix

--- src/containers/Snippet/Snippet.js
+++ src/containers/Snippet/Snippet.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Button from '../../components/Button/Button';
 import {connect} from 'react-redux';
+// import  { renderMessageSnippet, renderConversionSnippet } from './helperfunctions';
 
 class Snippet extends Component {
   
@@ -12,16 +13,17 @@ class Snippet extends Component {
     
     // TODO: move snippet creation into own function (e.g. function that returns the string)
     // TODO: move to another file as a helper function (since it's used in multiple places)
-    let snippet1 = "<script>var s = document.createElement('script'); s.src = 'http://localhost:9000/widget.js'; s.id = '123456'; s.setAttribute('data-config', '{'userId':" + " " + this.props.userId + ", 'position':" + " " + "'" + this.props.position + "'" + "}'); s.async = true; document.body.appendChild(s);</script>"
+    let messageSnippet = "<script>var s = document.createElement('script'); s.src = 'http://localhost:9000/widget.js'; s.id = '123456'; s.setAttribute('data-config', '{\"userId\": " + this.props.userId + ", \"position\": " + "\"" + this.props.position + "\"" + "}'); s.async = true; document.body.appendChild(s);</script>"
     
-    let snippet2 = "<script>var s = document.createElement('script'); s.src = 'http://localhost:3030/conversion.js'; s.id = '123456'; s.setAttribute('data-config', '{'email': [CONFIGURE], 'first_name': [CONFIGURE], 'last_name': [CONFIGURE], 'company_name': [CONFIGURE], 'conversion_event_id':" + " " + this.props.conversion_event_id + " 'user_id':" + " " + this.props.userId + "}'); s.async = true; document.body.appendChild(s);</script>"
+    let conversionSnippet = "<script>var s = document.createElement('script'); s.src = 'http://localhost:3030/conversion.js'; s.id = '123456'; s.setAttribute('data-config', '{\"email\": \"[CONFIGURE]\", \"first_name\": \"[CONFIGURE]\", \"last_name\": \"[CONFIGURE]\", \"company_name\": \"[CONFIGURE]\", \"conversion_event_id\": " + this.props.conversion_event_id + " \"user_id\": " + this.props.userId + "}'); s.async = true; document.body.appendChild(s);</script>"
     
     return (
+      
       <div>
         <h4>Step 2: Insert Snippet Into Code</h4>
         <div>
-          <p>{snippet1}</p>
-          <p>{snippet2}</p>
+          <p>{messageSnippet}</p>
+          <p>{conversionSnippet}</p>
         </div>
         <Button clicked={this.submitHandler}>Submit</ Button>
       </div>
