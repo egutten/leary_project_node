df69a98f538d98a2a5019f3bbe36a21384b0eed8
egutten <guttenberg.emily@gmail.com>
2019-10-11 16:09:04 -0700

    fixed snippets with correct url

--- src/shared/helperfunctions.js
+++ src/shared/helperfunctions.js
@@ -1,7 +1,7 @@
 export const renderMessageSnippet = (userId, conversion_event_id) => {
-  return "<!-- Leery Message Snippet --><script>var s = document.createElement('script'); s.src = 'http://getleery/widget.js'; s.id = '123456'; s.setAttribute('data-config', '{\"userId\": " + userId + "}'); s.async = true; document.body.appendChild(s);</script>"
+  return "<!-- Leery Message Snippet --><script>var s = document.createElement('script'); s.src = 'https://getleery.com/widget.js'; s.id = '123456'; s.setAttribute('data-config', '{\"userId\": " + userId + "}'); s.async = true; document.body.appendChild(s);</script>"
 };
 
 export const renderConversionSnippet = (userId, conversion_event_id) => {
-  return "<!-- Leery Conversion Snippet --><script>var s = document.createElement('script'); s.src = 'http://getleery/conversion.js'; s.id = '123456'; s.setAttribute('data-config', '{\"email\": \"[CONFIGURE]\", \"first_name\": \"[CONFIGURE]\", \"last_name\": \"[CONFIGURE]\", \"company_name\": \"[CONFIGURE]\", \"conversion_event_id\": " + conversion_event_id + ", \"user_id\": " + userId + "}'); s.async = true; document.body.appendChild(s);</script>"
+  return "<!-- Leery Conversion Snippet --><script>var s = document.createElement('script'); s.src = 'https://getleery.com/conversion.js'; s.id = '123456'; s.setAttribute('data-config', '{\"email\": \"[CONFIGURE]\", \"first_name\": \"[CONFIGURE]\", \"last_name\": \"[CONFIGURE]\", \"company_name\": \"[CONFIGURE]\", \"conversion_event_id\": " + conversion_event_id + ", \"user_id\": " + userId + "}'); s.async = true; document.body.appendChild(s);</script>"
 };
