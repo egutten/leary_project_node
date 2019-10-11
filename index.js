85498d1c42a6ea5dbd4a60fd85536a2208dcbf65
egutten <guttenberg.emily@gmail.com>
2019-10-11 11:39:22 -0700

    x

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ var dotenv            = require("dotenv").config(),
 
 //Allowed cors in localhost
 app.use(cors());
-app.options('*', cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
