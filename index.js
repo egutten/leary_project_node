0662a3651e0ce299b635bab500b1611470d961c8
egutten <guttenberg.emily@gmail.com>
2019-11-06 16:44:55 -0800

    backtracking on static change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var dotenv            = require("dotenv").config(),
 
 //Allowed cors in localhost
 app.use(cors());
-app.use('/', express.static(__dirname + '/'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
